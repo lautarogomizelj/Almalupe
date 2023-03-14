dd3d37b97593a4fb9ba11f973f42be478616242d
Lautaro Gomizelj <lautigomi@gmail.com>
2023-03-13 22:28:12 -0300

    Arreglo de navbar y agregado de slider

--- Almalupe (Proyecto nuevo)/animacionesIndex.js
+++ Almalupe (Proyecto nuevo)/animacionesIndex.js
@@ -1,8 +1,6 @@
 const navbar = document.querySelector(".seccion.abajo");
 const logo = document.getElementById("logoAlmalupe");
 
-console.log(navbar)
-
 var distanciaTopPagina;
 var offsetWidthNavBar;
 
@@ -36,3 +34,67 @@ function myFunction() {
     }
 }
 window.onscroll = function() {myFunction()};
+
+/*----------------------------------------------------*/
+
+
+let items = document.querySelectorAll('.carousel-item');
+let currentItem = 0;
+const itemsPerPage = 4;
+let itemWidth = calculateItemWidth();
+
+    function calculateItemWidth() {
+        return items[0].getBoundingClientRect().width + parseInt(window.getComputedStyle(items[0]).marginRight);
+    }
+
+    function updateCarousel() {
+        itemWidth = calculateItemWidth();
+        items.forEach((item, index) => {
+            item.style.transform = `translateX(-${currentItem * itemWidth}px)`;
+        });
+    }
+
+    function moveToNextItem() {
+        clearInterval(interval)
+        
+        if (currentItem < items.length - itemsPerPage) {
+            currentItem += itemsPerPage;
+        } else {
+            currentItem = 0;
+        }
+        updateCarousel();
+
+        interval = setInterval(moveToNextItem, 4000)
+    }
+
+    function moveToPrevItem() {
+        clearInterval(interval)
+
+        if (currentItem > 0) {
+            currentItem -= itemsPerPage;
+        } else {
+            currentItem = items.length - itemsPerPage;
+        }
+        updateCarousel();
+
+        interval = setInterval(moveToNextItem, 4000)
+    }
+
+
+    let prevButton = document.querySelector('.carousel-prev');
+    let nextButton = document.querySelector('.carousel-next');
+
+    prevButton.addEventListener('click', moveToPrevItem);
+    nextButton.addEventListener('click', moveToNextItem);
+
+    // window.addEventListener('resize', updateCarousel hola);
+
+    window.addEventListener('resize',() => {    
+        updateCarousel()
+   });
+    
+
+    //haciendo el codigo para que se mueva automaticamente
+
+    interval = setInterval(moveToNextItem, 4000)
+
